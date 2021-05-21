76645d2dda9689bcf384415e3080625a53a2e0c5
LongN22 <lnguyen7274@gmail.com>
2021-05-21 10:47:24 -0500

    Added weather function

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const discord  = require('discord.js');
 const client = new discord.Client();
 const PREFIX = '!';
+const axios = require('axios');
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
@@ -10,15 +11,37 @@ client.on('ready', ()=> {
     console.log('MyBot ONLINE');
 });
 
-client.on('message', msg =>{
+client.on('message', async msg =>{
     if(msg.content == 'test'){
         msg.reply("test");
         msg.react("❤️");
     }
 
     if(msg.content.startsWith(PREFIX)){
-        const [CMD, test] = msg.content.trim().substring(1).split(/\s+/);
-        msg.channel.send(test);
-        console.log(test);
+        const [COMMAND, ...args] = msg.content.trim().substring(1).split(/\s+/);
+        //msg.channel.send(test);
+        //console.log(arges);
+
+        if(COMMAND == "weather"){
+            let getWeather = async () => {
+                if(!args[0]){
+                    msg.reply("please put city");
+                    return;
+                }
+
+                cityName = args[0];
+                for (i = 1; i < args.length; i++) {
+                    cityName = cityName + " " + args[i];
+                }
+
+                let response = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + (process.env.WEATHER_API));
+                let temp = response.data;
+                return temp;
+            }
+            const res = await getWeather();
+            console.log(res);
+            msg.reply(Math.round((res.main.temp - 273.15) * 9/5 + 32) + "°F");
+        }
     }
+    
 });
